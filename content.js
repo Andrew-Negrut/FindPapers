905cecbe2e25d5c3b688aca117b041cce04b4bd0
Andrew Negrut <45469110+Andrew-Negrut@users.noreply.github.com>
2023-10-06 14:00:55 -0500

    Delete content.js

--- content.js
+++
@@ -1,7 +0,0 @@
-console.log("hi");
-document.addEventListener('mouseup', () => {
-    const selectedText = window.getSelection().toString();
-    if (selectedText.length > 0) {
-        chrome.runtime.sendMessage({ selectedText }); 
-    }
-});
